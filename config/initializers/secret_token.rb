f2e7903c342e5ced3ebb252646a8cedb642427bd
William Nelson <bnelson@arabidopsis.org>
2012-01-10 16:12:47 -0800

    updated .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BiosensorApp::Application.config.secret_token = '98654d785bd7fe6e28485133f76d697d333a9b692c0ad0bfdec74b5aceab9b6bc61fd8c72e491a3407a6723b5c560247f76bb2dd4d6a56be15119e33bbce4f90'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BiosensorApp::Application.config.session_store :cookie_store, :key => '_biosensor_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BiosensorApp::Application.config.session_store :active_record_store
